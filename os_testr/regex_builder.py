289b7e3d38a805319211fe9c05080e889f128cf8
Masayuki Igawa <masayuki@igawa.me>
2017-02-21 12:26:13 -0500

    Fix error when without --regex option
    This commit fixes the error when executing without --regex option.
    
    Closes-bug: #1666569
    Change-Id: Iee7037328e41c1087b87219dac4256d50784ae64

--- os_testr/regex_builder.py
+++ os_testr/regex_builder.py
@@ -19,7 +19,10 @@ import subprocess
 
 def _get_test_list(regex, env=None):
     env = env or copy.deepcopy(os.environ)
-    proc = subprocess.Popen(['testr', 'list-tests', regex], env=env,
+    testr_args = ['testr', 'list-tests']
+    if regex:
+        testr_args.append(regex)
+    proc = subprocess.Popen(testr_args, env=env,
                             stdout=subprocess.PIPE, universal_newlines=True)
     out = proc.communicate()[0]
     raw_test_list = out.split('\n')
